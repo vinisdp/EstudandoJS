d532d8509192f2cae7847d39793cdbf638c07313
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-03 17:51:14 -0400

    feat: criando codigos aula 5

---
+++ Aula5/script.js
@@ -0,0 +1,11 @@
+//criando função
+function funcao() {
+    console.log("Essa mensagem veio de uma função");
+}
+funcao();
+
+function mensagem(primeira, segunda){
+    console.log(primeira, segunda);
+}
+
+mensagem('Tudo certo ', 'jovem!');
